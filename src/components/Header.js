3882f238e9b0c93975c0d22a4e213b1e21e23556
dshinde <dnyaneshwarshinde33368@gmail.com>
2023-10-09 18:45:06 +0530

    UI design commited

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
     <div className="Container">
       <div className="clg_logo_cnt">
         <img className="clg_logo"
-          src="https://mis.gcekarad.ac.in/GCEKMIS/IMAGES/GCEK_logo_new1.png"
+          // src="https://mis.gcekarad.ac.in/GCEKMIS/IMAGES/GCEK_logo_new1.png"
+          src="https://th.bing.com/th/id/OIP.FqZ3XFU5_SDypeCK6QMDsgHaHa?pid=ImgDet&rs=1"
           alt="logo"
         />
       </div>
