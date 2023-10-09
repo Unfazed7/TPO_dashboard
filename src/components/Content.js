3504f9aee2e6a905987d9d8c3ad0c35f8cfc1cee
dshinde <dnyaneshwarshinde33368@gmail.com>
2023-10-09 17:38:25 +0530

    Commited simple UI

---
+++ src/components/Content.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './CSS/Content.css'
+let stu_cnt=0;
+const Content = () => {
+  return (
+    <div className='contnet_cnt'>
+        <div className="card_cnt">
+            <div className="card" id="card1">
+                <h1 id="placement_cnt">0</h1>
+                <h4>Total Placement Count</h4>
+            </div>
+            <div className="card" id="card1">
+                <h1>20LPA</h1>
+                <h4>Highest Package Offered</h4>
+            </div>
+            <div className="card" id="card1">
+                <h1>6LPA</h1>
+                <h4>Average Package Offered</h4>
+            </div>
+        </div>
+        <div className='stu_cnt'>
+            <div className="tbl_cnt">
+                <table className='tbl'rules="none">
+                    <tr>
+                        <th>Sr NO.</th>
+                        <th>Name</th>
+                        <th>Package</th>
+                        <th>Batch</th>
+                        <th>Branch</th>
+                    </tr>
+                </table>
+                <div className='add_new_stu'>
+                    <button id='add_new_stu_btn' onClick={func}>
+                        Add New Student
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+// document.getElementById('')
+function func(){
+    stu_cnt++;
+    const htmlData=`
+    <tr className='row'>
+    <td>${stu_cnt}</td>
+    <td>Sujay Asabe</td>
+    <td>50LPA</td>
+    <td>2024-2025</td>
+    <td>IT</td>
+    </tr>
+    `;
+    const tbl=document.querySelector('.tbl');
+    tbl.insertAdjacentHTML('beforeend',htmlData);
+    // console.log('event fired');
+    document.getElementById('placement_cnt').innerHTML=stu_cnt;
+}
+
+export default Content
---
+++ src/components/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './CSS/Sidebar.css'
+const Sidebar = () => {
+  return (
+    <div className='Side_bar_cont'>
+        <div className="filter">
+            <div className="Dept">
+                <select name="" className='Dropdown' id="dept-Dropdown">
+                    <option value="IT" className='options'>All Departments</option>
+                    <option value="IT" className='options'>Information Technology</option>
+                    <option value="IT" className='options'>Mechanichal</option>
+                    <option value="IT" className='options'>Electrical</option>
+                    <option value="IT" className='options'>Civil</option>
+                    <option value="IT" className='options'>Electronics</option>
+                </select>
+            </div>
+            <div className="Year">
+                <select name="" className='Dropdown' id="dept-Dropdown">
+                    <option value="IT" className='options'>Current Batch</option>
+                    <option value="IT" className='options'>2021-2022</option>
+                    <option value="IT" className='options'>2020-2021</option>
+                    <option value="IT" className='options'>2019-2020</option>
+                    <option value="IT" className='options'>2018-2019</option>
+                    <option value="IT" className='options'>2017-2018</option>
+                </select>
+            </div>
+        </div>
+        <div className='Upcoming_evnt'>
+            <h2>Upcoming Events...</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
